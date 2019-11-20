b6431c4e3cb1a168c1c2e0c732bdbc8f98db75f5
Topher Dao <td.topherdao@gmail.com>
2019-11-20 15:54:07 -0500

    Initial commit

---
+++ src/test/java/us/daofamily/demo/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package us.daofamily.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
