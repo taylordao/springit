c999d7d34ed2913447b0c051d67d186c505e83a2
Topher Dao <td.topherdao@gmail.com>
2019-12-07 12:41:50 -0500

    Adding mapping from link to comments

--- src/main/java/us/daofamily/springit/model/Comment.java
+++ src/main/java/us/daofamily/springit/model/Comment.java
@@ -7,6 +7,7 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.util.Objects;
 
 @Entity
@@ -23,6 +24,8 @@ public class Comment {
 
 
     //link
+    @ManyToOne
+    private Link link;
 
     /*public Long getId() {
         return id;
--- src/main/java/us/daofamily/springit/model/Link.java
+++ src/main/java/us/daofamily/springit/model/Link.java
@@ -7,6 +7,9 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -21,7 +24,11 @@ public class Link {
     @NonNull
     private String url;
 
+
+
     //comments
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<Comment>();
 
     /*public Long getId() {
         return id;
